8e3bdf06eea64673f779fa6727027aacb871ddf8
Steven Dove <dostev@aol.com>
2022-08-15 11:33:38 +0100

    Add project files.

---
+++ FollowingCourse/Data/Grocery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FollowingCourse
+{
+
+    public class Grocery
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should be less than 15")]
+        public string? Name { get; set; }
+
+        [Required]
+        [Range(1,1000, ErrorMessage = "Please Enter A Valid Price Range")]
+        public float price { get; set; }
+    }
+
+}
+
---
+++ Grocery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Grocery
+{
+    public class Grocery
+    {
+
+        public string Name { get; set; }
+        public float price { get; set; }
+    }
+}
